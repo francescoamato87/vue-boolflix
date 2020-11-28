df8bf8244e662f0e246b670636494d18cdea8680
FrancescoAmato87 <francesco.duff@gmail.com>
2020-11-28 16:39:03 +0100

    ToggleMenu-SearchBar-Buttons-

--- javascript.js
+++ javascript.js
@@ -94,3 +94,25 @@ const app = new Vue({
 
     }
 });
+
+// Jquery Section
+
+$(document).ready(function() {
+
+// Refs
+var dropLinks = $('.with-dropdown > a' );
+var dropMenu = $('.with-dropdown > .dropdown-menu');
+
+// Show / Hide dropdown Menu
+dropLinks.click( function() {
+
+  var actualMenu = $(this).next('.dropdown-menu');
+
+  dropMenu.not(actualMenu).hide();
+
+  actualMenu.toggle(1000);
+
+
+    });
+
+}); // <-- end doc ready
